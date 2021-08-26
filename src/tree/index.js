284d456086add4582c958c3ee0a3c2902eb85727
@sanchezalan97 <sanchezalan95@gmail.com>
2021-08-26 13:24:48 -0500

    Add code structure without using the entity for the non breaking space

--- src/tree/index.js
+++ src/tree/index.js
@@ -4,14 +4,30 @@ export default function Tree() {
   return (
     <div className="tree">
       <p>root</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;ant</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;bear</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cat</p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog</p>
-      <p>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant
-      </p>
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;frog</p>
+      <ol>
+        <li>
+          <p>ant</p>
+        </li>
+        <li>
+          <p>bear</p>
+          <ol>
+            <li>
+              <p>cat</p>
+            </li>
+            <li>
+              <p>dog</p>
+              <ol>
+                <li>
+                  <p>elephant</p>
+                </li>
+              </ol>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>frog</p>
+        </li>
+      </ol>
     </div>
   );
 }
